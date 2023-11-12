edd8fe3ef0a882fd4d58c87153ee36995c5f288d
Tamicktom <rickpokemon38@gmail.com>
2023-11-12 15:55:12 -0300

    asdasdasd

--- src/pages/my-profile/edit/index.tsx
+++ src/pages/my-profile/edit/index.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { Question, UserCircle, ArrowSquareRight, Check, X, Plus, GraduationCap, Bank, BookBookmark, CaretLeft } from "@phosphor-icons/react";
+import { Question, UserCircle, ArrowSquareRight, Check, X, Plus, GraduationCap, Bank, BookBookmark } from "@phosphor-icons/react";
 
 export default function MyProfile() {
   const router = useRouter();
@@ -31,20 +31,23 @@ export default function MyProfile() {
                 />
               </div>
               <div className="flex flex-col items-center justify-center w-full gap-2">
-                <p className="w-full text-2xl font-medium text-center">Lucas Gabriel de Souze</p>
-                <p className="w-full text-xl font-light text-center">lucas.gg2201@gmail.com</p>
+                <input type="text" defaultValue="Lucas Gabriel de Souze" className="w-full p-2 text-2xl font-medium text-center border rounded-lg border-neutral-300 focus:outline-neutral-500" />
+                <input type="email" defaultValue="lucas.gg2201@gmail.com" className="w-full p-2 text-xl font-light text-center border rounded-lg border-neutral-300 focus:outline-neutral-500" />
               </div>
               <div className="flex flex-col items-center justify-center w-full gap-4">
                 <h2 className="w-full text-2xl font-medium text-start">
                   Habilidades
                 </h2>
                 <div className="flex flex-row flex-wrap w-full gap-1">
-                  <Tag editable={false} value={0}>React</Tag>
-                  <Tag editable={false} value={8}>Next.js</Tag>
-                  <Tag editable={false} value={7}>Typescript</Tag>
-                  <Tag editable={false} value={10}>Node.js</Tag>
-                  <Tag editable={false} value={4}>Express</Tag>
-                  <Tag editable={false} value={5}>PostgreSQL</Tag>
+                  <Tag editable value={0}>React</Tag>
+                  <Tag editable value={8}>Next.js</Tag>
+                  <Tag editable value={7}>Typescript</Tag>
+                  <Tag editable value={10}>Node.js</Tag>
+                  <Tag editable value={4}>Express</Tag>
+                  <Tag editable value={5}>PostgreSQL</Tag>
+                  <button className="text-2xl">
+                    <Plus />
+                  </button>
                 </div>
               </div>
             </div>
@@ -56,25 +59,25 @@ export default function MyProfile() {
                 <div className="flex flex-col items-center justify-center w-full gap-2">
                   <div className="flex flex-row items-center justify-center w-full gap-2">
                     <GraduationCap className="text-2xl" />
-                    <p className="w-full">Banana</p>
+                    <input type="text" className="w-full p-2 border rounded-lg border-neutral-300 focus:outline-neutral-500" defaultValue="banana" />
                   </div>
                   <div className="flex flex-row items-center justify-center w-full gap-2">
                     <Bank className="text-2xl" />
-                    <p className="w-full">Banana</p>
+                    <input type="text" className="w-full p-2 border rounded-lg border-neutral-300 focus:outline-neutral-500" defaultValue="banana" />
                   </div>
                   <div className="flex flex-row items-center justify-center w-full gap-2">
                     <BookBookmark className="text-2xl" />
-                    <p className="w-full">Banana</p>
+                    <input type="text" className="w-full p-2 border rounded-lg border-neutral-300 focus:outline-neutral-500" defaultValue="banana" />
                   </div>
                 </div>
               </div>
               <div className="flex flex-col w-full h-full gap-3 p-4 bg-white rounded-md">
                 <h2 className="capitalize">Experiências</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum facere officiis voluptas optio, earum recusandae nobis illum dolores! Distinctio consequatur culpa vero error fugit harum dolore aperiam dignissimos quis asperiores.</p>
+                <textarea name="experience" id="" cols={30} rows={4} className="p-2 border rounded-lg border-neutral-300 focus:outline-neutral-500" defaultValue="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum facere officiis voluptas optio, earum recusandae nobis illum dolores! Distinctio consequatur culpa vero error fugit harum dolore aperiam dignissimos quis asperiores." />
               </div>
               <div className="flex flex-col w-full h-full gap-3 p-4 bg-white rounded-md">
                 <h2 className="capitalize">Sobre mim</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum facere officiis voluptas optio, earum recusandae nobis illum dolores! Distinctio consequatur culpa vero error fugit harum dolore aperiam dignissimos quis asperiores.</p>
+                <textarea name="experience" id="" cols={30} rows={4} className="p-2 border rounded-lg border-neutral-300 focus:outline-neutral-500" defaultValue="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum facere officiis voluptas optio, earum recusandae nobis illum dolores! Distinctio consequatur culpa vero error fugit harum dolore aperiam dignissimos quis asperiores." />
               </div>
             </div>
           </div>
@@ -144,10 +147,13 @@ function Header() {
   return (
     <div className="flex flex-row w-full py-12">
       <div className="flex flex-row gap-4 px-6 py-2 text-2xl text-black bg-white rounded-lg">
+        <span className="text-2xl font-medium">Editar Perfil</span>
         <button>
-          <CaretLeft />
+          <Check />
         </button>
-        <span className="text-2xl font-medium">Voltar</span>
+        <button>
+          <X />
+        </button>
       </div>
     </div>
   );
